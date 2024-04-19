596fdb88e8a36ee56b8ccbe8aa27112008899bbc
Noam-Alum <noam.alum@gmail.com>
2024-04-20 02:31:07 +0300

    Initial commit

--- install-hugo.sh
+++ install-hugo.sh
@@ -2,13 +2,18 @@
 #
 # Install hugo && npm dependencies
 
+# Highlights
+Info="[\e[36mINFO\e[0m]"
+Error="[\e[31mERROR\e[0m]"
+
 # check if user is running as root
 if [ `id -u` -ne 0 ]; then
-    echo "Please run as root!"
+    echo -e "$Error Please run as root!"
     exit 1
 fi
 
 # check if hugo/npm is installed
+echo -e "$Info This might take a few minutes, be patient."
 dependencies=("hugo" "npm")
 
 # Get package manager
@@ -26,21 +31,22 @@ do
     fi
 done
 
+echo -e "$Info Updating package manager..."
 $PackageManager update &> /dev/null
 
 for d in ${dependencies[@]}
 do
     if [ ! `which $d` ]; then
-        echo "Dependency \"$d\" missing, installing now."
+        echo -e "$Info Dependency \"$d\" missing, installing now."
 
         # install dependency
-        $PackageManager install -y $d & /dev/null || echo "Installation of \"$d\" was not successful."
+        $PackageManager install -y $d &> /dev/null || { echo -e "$Error Installation of \"$d\" was not successful."; exit 1; }
     fi
 done
 
 # install npm dependencies
-echo "installing npm dependencies"
-npm install &> /dev/null || echo "Installation of \"npm dependencies\" was not successful."
+echo -e "$Info Installing npm dependencies..."
+npm install && npm update &> /dev/null || { echo -e "$Error Installation of \"npm dependencies\" was not successful."; exit 1; }
 
-# start hugo server
-hugo server
+# how to start
+echo -e "$Info Run \"hugo server\" to view the website."
