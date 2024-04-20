9712806bcb61c1e4093ccff0d65baae3dd651299
Noam-Alum <noam.alum@gmail.com>
2024-04-20 04:59:37 +0300

    Initial commit

--- install-hugo.ps1
+++ install-hugo.ps1
@@ -27,20 +27,7 @@ if (-not (CommandExists "npm")) {
 # Install Hugo if not already installed
 if (-not (CommandExists "hugo")) {
     Write-Info "Installing Hugo..."
-    winget install Hugo
-}
-
-# Install Node.js using NVM if Node.js version 20.5.0 is not installed
-$desiredNodeVersion = "20.5.0"
-if (-not (CommandExists "node" -and (node -v) -eq "v$desiredNodeVersion")) {
-    Write-Info "Installing Node.js version $desiredNodeVersion using NVM..."
-    $env:NVM_HOME = "$env:USERPROFILE\.nvm"
-    $env:NVM_SYMLINK = "$env:USERPROFILE\AppData\Roaming\nvm"
-    $nvmUrl = "https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.ps1"
-    Invoke-WebRequest -Uri $nvmUrl -OutFile "$env:TEMP\nvm-install.ps1"
-    & "$env:TEMP\nvm-install.ps1" | Invoke-Expression
-    nvm install $desiredNodeVersion
-    nvm use $desiredNodeVersion
+    winget install Hugo.Hugo.Extended
 }
 
 # Install npm dependencies for the project
